5321fec96d3bbd869b36420fb51dbfb68db36c11
snaramirez872 <seanramirez818@gmail.com>
2025-06-14 13:31:34 -0400

    moved destination of test data up one level

--- scripts/source.py
+++ scripts/source.py
@@ -5,5 +5,5 @@ df = pd.read_csv("../data/test-data/spreadsheets/dummy.csv")
 
 dat = df.to_dict(orient="records")
 
-with open("../data/test-data/test.json", "w", encoding="utf-8") as f:
+with open("../data/test.json", "w", encoding="utf-8") as f:
     json.dump(dat, f, indent=2)
